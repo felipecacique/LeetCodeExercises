77129c06831853ec99802ba392f47e68f3cb79e5
FIRST_NAME LAST_NAME felipecacique <felipecacique2@hotmail.com>
2024-05-16 12:52:32 -0300

    update

--- medium_new/remove-nodes-from-linked-list.py
+++ medium_new/remove-nodes-from-linked-list.py
@@ -6,14 +6,15 @@
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # https://leetcode.com/problems/remove-nodes-from-linked-list/?envType=daily-question&envId=2024-05-06
-
+        # itereate backwards. Go to the end of the list by a dfs, post orderr, and return the node (backwards). In post order we append the next_node to the current node(head) by head.next = node only if the head.val > next_node.val. Otherswise we must remove the current node from the list, and just return the next_node. It satisfies the rule and at the nd we will have he function returning the linked list answer.
+        
         if not head.next:
             return head
         
-        node = self.removeNodes(head.next)
+        next_node = self.removeNodes(head.next)
         
-        if node.val > head.val: # then remove head
-            return node
+        if next_node.val > head.val: # then remove head
+            return next_node
         
-        head.next = node
+        head.next = next_node
         return head
