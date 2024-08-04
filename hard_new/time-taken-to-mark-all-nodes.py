c6446e982da956738decb35c840cc65ad24dc86e
FIRST_NAME LAST_NAME felipecacique <felipecacique2@hotmail.com>
2024-08-03 22:07:34 -0300

    update

--- hard_new/time-taken-to-mark-all-nodes.py
+++ hard_new/time-taken-to-mark-all-nodes.py
@@ -18,6 +18,7 @@ class Solution:
         solutionFromTop = [0] * n
 
         def getSolutionFromButtom(node):
+            # Get the solution from the bottom children, post order traversal, (button up approach), using dp. The solution of node sol(node) = mox(sol(children), ...). And we have to add something to each sol(children) that is 1 if children is odd and 2 if it is even.
 
             if not node in adj:
                 solutionFromButtom[node] = 0
@@ -25,7 +26,7 @@ class Solution:
 
             for neighbour in adj[node]:
                 solButtom = getSolutionFromButtom(neighbour)
-                solButtom = solButtom + 2 if neighbour % 2 == 0 else solButtom + 1
+                solButtom = solButtom + 2 if neighbour % 2 == 0 else solButtom + 1 # apply the rule. So the solution toist node from the path neighbour1, is the solution of neighbour1 plus 1 or 2, deppending is neighbour1 is odd or even
                 solutionFromButtomDict[(node, neighbour)] = solButtom
                 solutionFromButtom[node] = max(solutionFromButtom[node], solButtom)
             
@@ -33,15 +34,18 @@ class Solution:
 
 
         import heapq
-        
+
         def getSolutionFromTop(node, solTop):
+            # Top down approach, pre order traversal. Each node receives the solution from the parent node, and we also have the solution from every children node (from the bottom) gotten previosly.
+            # so the solution of the node, is the max between the solution of the parent and the soluion of every children paths.
+            # the node must send its solution to the children ith (solNode) added 1 or 2 deppending if node is even or odd. But this solution solNode we will send to the children ith must have excluded in its calculation the path from ith.  
             
             if not node in adj:
                 solutionFromTop[node] = solTop
                 solution[node] = solutionFromTop[node]
                 return
 
-            # get the max and the second max from neighbours
+            #this chunck of code gets the max and the second max from neighbours. This is just to make it more time efficient when excluding the neighbour path from solNode
             solNeighbours = []
             for neighbour in adj[node]:
                 solNeighbour = solutionFromButtomDict[(node, neighbour)] 
@@ -55,10 +59,12 @@ class Solution:
             for neighbour in adj[node]:
                 solNode = solTop
                 solNeighbour = solutionFromButtomDict[(node, neighbour)]
+                # Exclude the neighbout node from the max calculation. Instead of doing a new for and doing the max of all neighbours except neighbour ith, we can just do as follow. No need for and extre loop.
                 if solNeighbour != maxSolutionFromButtom: solNode = max(solNode, maxSolutionFromButtom)
                 else: solNode = max(solNode, maxSolutionFromButtom2)
+                
                 solutionFromTop[node] = solNode
-                solution[node] = max(solutionFromTop[node], solutionFromButtom[node])
+                solution[node] = max(solutionFromTop[node], solutionFromButtom[node]) # get the max time from every edge, from the bottoms and from the tops
 
                 getSolutionFromTop(neighbour, solNode + 2 if node % 2 == 0 else solNode + 1)
 
