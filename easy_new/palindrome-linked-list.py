c1e8b67ebe9aec0a29b0f42b1b9830cb37c7e686
FIRST_NAME LAST_NAME felipecacique <felipecacique2@hotmail.com>
2024-08-28 19:43:40 -0300

    update

--- easy_new/palindrome-linked-list.py
+++ easy_new/palindrome-linked-list.py
@@ -9,23 +9,30 @@ class Solution:
         # time O(n) space o(1)
         # find the middle of the list, reverse the second half, compare
 
+        if not head: return False
+        if not head.next: return True
+
         # find the middle
         fast, slow = head, head
-        prev = None
-        while fast:
-            prev = slow
+        cut = None
+        while fast and fast.next:
+            cut = slow
             slow = slow.next
             fast = fast.next.next
 
         middle = slow
         
         # reverse the second half
+        prev = None
         while slow:
             nex = slow.next
             slow.next = prev
             prev = slow
             slow = nex
+        # cut.next = prev
+        middle = prev # update the middle
         
+
         # compare the nodes of the first half with the second half
         node = head
         while middle:
