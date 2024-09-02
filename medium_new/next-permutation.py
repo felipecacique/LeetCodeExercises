35382ed88c4f7b91179a1b5014d2f5ea21066c59
FIRST_NAME LAST_NAME felipecacique <felipecacique2@hotmail.com>
2024-09-02 11:57:23 -0300

    update

---
+++ medium_new/next-permutation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # o(n) o(n)
+        exchanged = False
+        maxNum = nums[-1]
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                idx = nums[i+1]
+                for j in range(i+1, len(nums)):
+                    if nums[j] <= nums[i]:
+                        break
+                    idx = j
+                nums[i], nums[idx] = nums[idx], nums[i]
+                nums[i+1:] = reversed(nums[i+1:])
+                exchanged = True
+                break
+        
+        if not exchanged:
+            nums.reverse()
+            
