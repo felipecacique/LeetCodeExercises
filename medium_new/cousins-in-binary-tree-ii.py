fe9fb45b22d766d9e90fd737477d8d15c497efee
FIRST_NAME LAST_NAME felipecacique <felipecacique2@hotmail.com>
2024-11-22 17:32:53 -0300

    update

--- medium_new/cousins-in-binary-tree-ii.py
+++ medium_new/cousins-in-binary-tree-ii.py
@@ -13,29 +13,64 @@ class Solution:
                 return
             
             if not h in table: table[h] = {"total":0}
-            if not parent in table[h]: table[h][parent] = []
-            table[h][parent].append(node.val)
             table[h]["total"] += node.val
 
             createTable(node.left, node, h+1)
             createTable(node.right, node, h+1)
         
-        def changeTree(node, parent, h):
+        def changeTree(node, valBrother, parent, h):
             if not node:
                 return
             
-            sum_cousin = table[h]["total"] 
-            if h in table and parent in table[h]:
-                sum_cousin -= sum(table[h][parent])
-
+            sum_cousin = table[h]["total"] - node.val
+            if valBrother: sum_cousin -= valBrother
             node.val = sum_cousin
 
-            changeTree(node.left, node, h+1)
-            changeTree(node.right, node, h+1)
+            node_right_val = node.right.val if node.right else 0
+            node_left_val = node.left.val if node.left else 0
+
+            changeTree(node.left, node_right_val, node, h+1)
+            changeTree(node.right, node_left_val,  node, h+1)
         
         table = {}
         createTable(root, 0, 0)
-        changeTree(root, 0, 0)
+        changeTree(root, 0, 0, 0)
 
         return root
+
+
+
+
+        # def createTable(node, parent, h):
+        #     if not node:
+        #         return
+            
+        #     if not h in table: table[h] = {"total":0}
+        #     if not parent in table[h]: table[h][parent] = []
+        #     table[h][parent].append(node.val)
+        #     table[h]["total"] += node.val
+
+        #     createTable(node.left, node, h+1)
+        #     createTable(node.right, node, h+1)
+        
+        # def changeTree(node, parent, h):
+        #     if not node:
+        #         return
+            
+        #     sum_cousin = table[h]["total"] 
+        #     if h in table and parent in table[h]:
+        #         sum_cousin -= sum(table[h][parent])
+
+        #     node.val = sum_cousin
+
+        #     changeTree(node.left, node, h+1)
+        #     changeTree(node.right, node, h+1)
+        
+        # table = {}
+        # createTable(root, 0, 0)
+        # changeTree(root, 0, 0)
+
+        # return root
+
+
 
